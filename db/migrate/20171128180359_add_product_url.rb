34aca60d909572266181799cba70e5668af37604
nikitazlain <kilimangara@yandex.ru>
2017-11-28 21:05:10 +0300

    produt url

---
+++ db/migrate/20171128180359_add_product_url.rb
@@ -0,0 +1,5 @@
+class AddProductUrl < ActiveRecord::Migration[5.1]
+  def change
+    add_column :products, :url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171126152239) do
+ActiveRecord::Schema.define(version: 20171128180359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20171126152239) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
   end
 
   create_table "users", force: :cascade do |t|
