ddeebcd2e622bd250ee00ce1fec7d67f7dffaecf
nikitazlain <kilimangara@yandex.ru>
2017-12-10 21:04:47 +0300

    parent category is not required

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
 
   has_and_belongs_to_many :products
 
-  belongs_to :parent_category, class_name: 'Category'
+  belongs_to :parent_category, class_name: 'Category', required: false
 
   has_many :inner_categories, class_name: 'Category', foreign_key: 'parent_category_id'
 end
